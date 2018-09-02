aeecea5da6ca562d36f84721693c594a53cde09f
jeffwdoak <jeff.w.doak@gmail.com>
2018-09-02 12:41:15 +0000

    creating testing module at top level

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+"""
+Tests of exercise/models.py
+
+"""
+
+from django.test import TestCase
+
+from exercise.models import Exercise
+
+class ExerciseTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_exercise_has_correct_fields(self):
+        pass
+
+    def test_get_all_exercises(self):
+        pass
+
+
